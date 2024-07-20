fd3c58a4addb6b83029ad67dfa9c8251c0c753fe
u7284920 <u7284920@anu.edu.au>
2024-07-20 18:31:50 +1000

    new deploy

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,7 +2,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
-      <p>Navigate over to <a href="/test"><code>/test</code> to test Netlify Forms</a></p>
+      <p>Navigate over t o <a href="/test"><code>/test</code> to test Netlify Forms</a></p>
     </div>
   );
 }
